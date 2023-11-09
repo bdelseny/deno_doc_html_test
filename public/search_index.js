2ce31ba062ccd50a9db4bcc0e263805ed7833563
Bastien Delseny <bastien.delseny@gmail.com>
2023-11-09 12:30:11 +0000

    deno doc local html

---
+++ public/search_index.js
@@ -0,0 +1,3 @@
+(function () {
+  window.DENO_DOC_SEARCH_INDEX = {"nodes":[{"kind":"function","name":"doesNothing","nsQualifiers":"","location":{"filename":"","line":9,"col":0},"declarationKind":"private"},{"kind":"class","name":"EmptyClass","nsQualifiers":"","location":{"filename":"","line":21,"col":0},"declarationKind":"private"}]};
+})()
