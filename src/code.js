96c74f724cbbf9e25e8c65f01afc591feb5207e0
Bastien Delseny <bastien.delseny@gmail.com>
2023-11-09 12:16:15 +0000

    Prepare for documentation

---
+++ src/code.js
@@ -0,0 +1,47 @@
+/**
+ * A function that only logs its parameters
+ * @property {string} str a string
+ * @property {number} int a number
+ * @property {boolean} bool a boolean
+ * @property {any[]} array an array
+ * @property {string|number|string[]} optional an optional value with many type possible
+ */
+function doesNothing(str, int, bool, array, optional="optional") {
+    console.table(str, int, bool, array, optional);
+}
+
+/**
+ * A class not doing much
+ * @property {string} str a string
+ * @property {number} int a number
+ * @property {boolean} bool a boolean
+ * @property {any[]} array an array
+ * @property {string|number|string[]} optional an optional value with many type possible
+ */
+class EmptyClass {
+    str;
+    int;
+    bool;
+    array;
+    optional="optional";
+    /**
+     * EmptyClass constructor
+     * @param {string} str a string
+     * @param {number} int a number
+     * @param {boolean} bool a boolean
+     * @param {any[]} array an array
+     * @param {string|number|string[]} optional an optional value with many type possible
+     */
+    constructor(str, int, bool, array, optional="optional") {
+        /** @type {string} */
+        this.str = str;
+        /** @type {number} */
+        this.int = int;
+        /** @type {boolean} */
+        this.bool = bool;
+        /** @type {any[]} */
+        this.array = array;
+        /** @type {string|number|string[]} */
+        this.optional = optional;
+    }
+}
